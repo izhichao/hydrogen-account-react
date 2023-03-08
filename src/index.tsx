cb93ba975fe8bea31ce409f3c15dbaac81a99cb9
izhichao <43055550@qq.com>
2023-03-08 12:27:34 +0800

    feat: 定义全局样式

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './router';
 import 'normalize.css';
-
+import './style/index.less';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
